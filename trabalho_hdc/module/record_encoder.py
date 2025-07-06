30d2de61d4b8cf8b4979ad82f7f8d0f132b3d127
MarinaPiragibe <marinapiragibe@id.uff.br>
2025-07-06 19:57:06 -0300

    car evaluation funconando mas a acuracia esta terrivel

--- trabalho_hdc/module/record_encoder.py
+++ trabalho_hdc/module/record_encoder.py
@@ -8,10 +8,8 @@ from binhd.functional import multibundle
 class RecordEncoder(nn.Module):
     def __init__(self, out_features, size, num_levels):
         super(RecordEncoder, self).__init__()
-        # Create random position vectors for each feature (field)
         self.position = embeddings.Random(size, out_features, vsa="BSC", dtype=torch.int8)
         self.value = ScatterCode(num_levels, out_features)
-
     
     def forward(self, x):
         sample_hv = torchhd.bind(self.position.weight, self.value(x))
